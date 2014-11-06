4f53be42e3bfd035a3d02e21e9f71285a5e343f6
nathanhd <duongnathan93@yahoo.com>
2014-11-05 18:33:06 -0800

    Fixed header errors

--- GoliathGame/GoliathGame/GameState.h
+++ GoliathGame/GoliathGame/GameState.h
@@ -2,12 +2,9 @@
 #include "State.h"
 #include "InputManager.h"
 #include "Player.h"
-<<<<<<< HEAD
 #include "Section.h"
 #include "Global.h"
-=======
 #include "Level.h"
->>>>>>> 819790a0b5f26e4b8bfd7f7e8e91a2ee419ec3d3
 
 class GameState : public State
 {
@@ -25,14 +22,11 @@ public:
 
 private:
 	InputManager inputManager;
-<<<<<<< HEAD
-	Section s;
+	//Section s;
 	sf::View view;
 	sf::RenderWindow win;
 	sf::Vector2f playerPos;
 	void viewCheck();
-=======
 	Level s;
->>>>>>> 819790a0b5f26e4b8bfd7f7e8e91a2ee419ec3d3
 };
 
