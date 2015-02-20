8ca7846f6b6ca344c6663a650757b4bde392b633
Tysterman74 <tynutsathitya@gmail.com>
2015-02-19 17:54:56 -0800

    Inserted in rotation and offsetting backgrounds.

--- GoliathGame/GoliathGame/Background.cpp
+++ GoliathGame/GoliathGame/Background.cpp
@@ -12,12 +12,18 @@ Background::Background(int levelNum, int roomNum)
 	{
 		Layer l;
 		l.image = sf::Sprite(*TextureManager::GetInstance().retrieveTexture(temp.movingLayers[i].imageName));
-		l.image.setPosition(0.0f, 0.0f);
+		//l.image.setPosition(0.0f, 0.0f);
+		l.image.setPosition(temp.movingLayers[i].posOffset.x, temp.movingLayers[i].posOffset.y);
+		l.image.setOrigin(l.image.getGlobalBounds().left/2,l.image.getGlobalBounds().top/2);
 		l.image.scale(2.0f, 2.0f);
 		l.scale = temp.movingLayers[i].scale;
+		l.degrees = temp.movingLayers[i].degrees;
+		l.timeToRotate = temp.movingLayers[i].timeToRotate;
+		l.currentDegrees = 0;
+		l.rotateForward = false;
 		movingLayers.push_back(l);
 	}
-	nonMovingLayer.setPosition(320.0f, 0.0f);
+	nonMovingLayer.setPosition(temp.posOffset.x, temp.posOffset.y);
 }
 
 
@@ -41,16 +47,44 @@ void Background::update(float deltaTime)
 {
 	for (int i = 0; i < movingLayers.size(); i++)
 	{
-		movingLayers[i].image.move(sf::Vector2f(vel.x * movingLayers[i].scale.x * deltaTime, vel.y * movingLayers[i].scale.y * deltaTime));
+		if (!movingLayers[i].rotateForward)
+			movingLayers[i].image.move(sf::Vector2f(vel.x * movingLayers[i].scale.x * deltaTime, vel.y * movingLayers[i].scale.y * deltaTime));
+		
+		//if (movingLayers[i].rotateForward) {
+		//	movingLayers[i].currentDegrees += deltaTime * movingLayers[i].degrees/movingLayers[i].timeToRotate;
+		//	movingLayers[i].image.rotate(deltaTime * movingLayers[i].degrees/movingLayers[i].timeToRotate);
+		//	if (movingLayers[i].currentDegrees > movingLayers[i].degrees)
+		//		movingLayers[i].rotateForward = false;
+		//}
+		//else {
+		//	movingLayers[i].currentDegrees -= deltaTime * movingLayers[i].degrees/movingLayers[i].timeToRotate;
+		//	movingLayers[i].image.rotate(-deltaTime * movingLayers[i].degrees/movingLayers[i].timeToRotate);
+		//	if (movingLayers[i].currentDegrees < 0)
+		//		movingLayers[i].rotateForward = true;
+		//}
+
 		if (movingLayers[i].image.getPosition().y + movingLayers[i].image.getGlobalBounds().height 
 			< nonMovingLayer.getPosition().y + nonMovingLayer.getGlobalBounds().height)
 		{
 			//reset();
 			vel.y *= -1;
+			//movingLayers[i].currentDegrees += deltaTime * movingLayers[i].degrees/movingLayers[i].timeToRotate;
+			//movingLayers[i].image.rotate(deltaTime * movingLayers[i].degrees/movingLayers[i].timeToRotate);
+			//if (movingLayers[i].currentDegrees > movingLayers[i].degrees) {
+			//	movingLayers[i].rotateForward = false;
+			//	vel.y *= -1;
+			//}
 		
 		}
 		else if (movingLayers[i].image.getPosition().y > nonMovingLayer.getPosition().y) {
 			vel.y *= -1;
+			
+			//movingLayers[i].currentDegrees -= deltaTime * movingLayers[i].degrees/movingLayers[i].timeToRotate;
+			//movingLayers[i].image.rotate(deltaTime * movingLayers[i].degrees/movingLayers[i].timeToRotate);
+			//if (movingLayers[i].currentDegrees > movingLayers[i].degrees) {
+			//	movingLayers[i].rotateForward = false;
+			//	vel.y *= -1;
+			//}
 		}
 	}
 }
